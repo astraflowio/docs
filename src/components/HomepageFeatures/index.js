890d887f33c3c604f1b2a27295b230caf7c8bf5a
aravindanr <1028385+aravindanr@users.noreply.github.com>
2024-01-26 19:31:21 -0800

    minimal home page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Open Source',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/icons/book-open.svg').default,
     description: (
       <>
         Apache-2.0 license for commercial and non-commerical use. Freedom to deploy, modify and contribute back.
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Modular',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Svg: require('@site/static/icons/grid.svg').default,
     description: (
       <>
         A fully abstracted backend enables you choose your own persistence layer and queueing service.
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: 'Control',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Svg: require('@site/static/icons/code.svg').default,
     description: (
       <>
         Powerful flow control constructs including Decisions, Dynamic Fork-Joins and Subworkflows. Variables and templates are supported.
@@ -32,7 +32,7 @@ const FeatureList = [
   },
   {
       title: 'Polyglot',
-      Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+      Svg: require('@site/static/icons/aperture.svg').default,
       description: (
         <>
           Client libraries in multiple languages allows workers to be implemented in Java, Node JS, Python and C#.
@@ -41,7 +41,7 @@ const FeatureList = [
     },
     {
       title: 'Scalable',
-      Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+      Svg: require('@site/static/icons/chevron-up.svg').default,
       description: (
         <>
           Distributed architecture for both orchestrator and workers scalable from a single workflow to millions of concurrent processes.
@@ -50,7 +50,7 @@ const FeatureList = [
     },
     {
           title: 'Conductor',
-          Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+          Svg: require('@site/static/icons/conductor.svg').default,
           description: (
             <>
               Fully compatible with Conductor. Workflows and Tasks can be used with no changes.
